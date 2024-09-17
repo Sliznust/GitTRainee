73ca377ac1cc101e9cb1f833e53c408afacb94d4
Sliznust <Ach123098@gmail.com>
2024-09-17 17:43:47 +0300

    first commit

---
+++ src/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+    public int x;
+    public int y;
+    public int color;
+    public void print()
+    {
+        System.out.println("Цвет точки x=" + x + ", y=" + y + " " + color);
+        System.out.println("1234");
+    }
+}
